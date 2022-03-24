d79ca850a0814a63fb3bde35204014c64f7e6bad
Hrishikesh Dnyaneshwar Dalimbkar <hrishikeshd@cybage.com>
2022-03-24 20:02:35 +0530

    changes

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -8,8 +8,22 @@ Ext.define("Sencha.app.view.main.MainController", {
   // need to specify alias because it will be search with this name from the main.js file
   alias: "controller.main",
 
-  onItemSelected: function (sender, record) {
-    Ext.Msg.confirm("Confirm", "Are you sure?", "onConfirm", this);
+  // onItemSelected: function (sender, record) {
+  //   Ext.Msg.confirm("Confirm", "Are you sure?", "onConfirm", this);
+  // },
+
+  // views: ["mainlist"],
+  control({
+    'useredit button[action=save]': {
+      click: "updateUser",
+  }
+  }),
+
+  editUser: function (grid, record) {
+    console.log("Double clicked on " + record.get("name"));
+    var view = Ext.widget("useredit");
+
+    view.down("form").loadRecord(record);
   },
 
   onConfirm: function (choice) {
